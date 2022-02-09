8aae03c45e2130fe752a903ac9f44813c724ce06
Denis-Pachekhon <denis.pachekhon.work@gmail.com>
2022-02-09 18:47:43 +0100

    CR-23: Implement tests for CalculateCreativeTime

--- backend/CreativityReportGenerator.Services.Abstractions/ICreativityReportGeneratorService.cs
+++ backend/CreativityReportGenerator.Services.Abstractions/ICreativityReportGeneratorService.cs
@@ -1,8 +1,6 @@
 ﻿using CreativityReportGenerator.Core.Models;
 using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CreativityReportGenerator.Services.Abstractions
 {
